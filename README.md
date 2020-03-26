da8bc06e6d17bed0bf026089985bf6abf01d8d98
FaLLeN <amitabhamajumder2@gmail.com>
2020-03-27 01:29:29 +0530

    updated

--- README.md
+++
@@ -1,1 +0,0 @@
-The project aim is to provide a nice mobile friendly view of the ongoing covid-19 pandemic in different formats like graph ,tables and geographical distributions of the confirmed cases.
---
+++ js/graph.js
@@ -0,0 +1,80 @@
+// confirmed 
+Plotly.d3.csv("confirmed_data.csv", function (err, rows) {
+  function unpack(rows, key) {
+    return rows.map(function(row) {
+      return row[key];
+    });
+  }
+
+  var trace1 = {
+    type: "scatter",
+    mode: "lines",
+    x: unpack(rows, "date"),
+    y: unpack(rows, "confirmed_cases"),
+    line: { color: "#17BECF" }
+  };
+
+
+  var data = [trace1];
+  var layout = {
+    title: "number of confirmed cases (india)"
+  };
+
+  Plotly.newPlot("confirmed", data, layout);
+});
+
+
+
+// death 
+
+
+Plotly.d3.csv("death_data.csv", function(err, rows) {
+  function unpack(rows, key) {
+    return rows.map(function(row) {
+      return row[key];
+    });
+  }
+
+  var trace1 = {
+    type: "scatter",
+    mode: "lines",
+    x: unpack(rows, "date"),
+    y: unpack(rows, "confirmed_cases"),
+    line: { color: "#17BECF" }
+  };
+
+
+  var data = [trace1];
+  var layout = {
+    title: "number of deaths (india)"
+  };
+
+  Plotly.newPlot("death", data, layout);
+});
+
+
+// recovery 
+
+Plotly.d3.csv("recovered_data.csv", function(err, rows) {
+  function unpack(rows, key) {
+    return rows.map(function(row) {
+      return row[key];
+    });
+  }
+
+  var trace1 = {
+    type: "scatter",
+    mode: "lines",
+    x: unpack(rows, "date"),
+    y: unpack(rows, "confirmed_cases"),
+    line: { color: "#17BECF" }
+  };
+
+
+  var data = [trace1];
+  var layout = {
+    title: "number of recovery (india)"
+  };
+
+  Plotly.newPlot("recovery", data, layout);
+});
