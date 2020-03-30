aa0e28f8aed55c074463bec10fce4ff1f5f9936f
FaLLeN <amitabhamajumder2@gmail.com>
2020-03-30 22:21:36 +0530

    gv

--- js/table.js
+++ js/table.js
@@ -20,16 +20,30 @@ $(document).ready(function() {
 
 // total 
 $(document).ready(function() {
-  $.getJSON("https://api.covid19india.org/data.json", function(data) {
-    var x = "";
-    $.each(data["statewise"], function(key, value) {
-      if (value.state == "Total") {
-        $("#Confirmed").append(value.confirmed);
-        $("#Death").append(value.deaths);
-        $("#Recovered").append(value.recovered);
-        $("#Updated").append(value.lastupdatedtime);
+  $.getJSON("https://api.covid19india.org/data.json", function (data) {
+    var y = 0;
+    var x = 0;
+    var z = 0;
+    $.each(data["cases_time_series"], function (key1, value1) {
+      if (value1.totalconfirmed) {
+        y = value1.totalconfirmed;
+        x = value1.totaldeceased;
+        z = value1.totalrecovered;
       }
     });
+      $.each(data["statewise"], function (key, value) {
+        if (value.state == "Total") {
+          $("#Confirmed").append(value.confirmed);
+          $("#Today_Confirmed").append(value.confirmed-y);
+          $("#Death").append(value.deaths);
+          $("#Today_Death").append(value.deaths-x);
+          $("#Recovered").append(value.recovered);
+          $("#Today_Recovered").append(value.recovered-z);
+          $("#Updated").append(value.lastupdatedtime);
+        }
+      });
+    
     
   });
 });
+
